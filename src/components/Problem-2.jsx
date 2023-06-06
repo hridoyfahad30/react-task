7aed380121dd6af4e1fc6a32298d4bf62c815847
hridoyfahad30 <hridoyfahad30@gmail.com>
2023-06-06 15:44:31 +0600

    Problem 2 Modal data fetch and even button work

--- src/components/Problem-2.jsx
+++ src/components/Problem-2.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
+import { Form } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 
@@ -7,11 +8,36 @@ const Problem2 = () => {
   const [showA, setShowA] = useState(false);
   const [showB, setShowB] = useState(false);
 
+  const [check, setCheck] = useState(true);
+
   const handleCloseA = () => setShowA(false);
   const handleShowA = () => setShowA(true);
 
   const handleCloseB = () => setShowB(false);
   const handleShowB = () => setShowB(true);
+
+  const [allContact, setAllContact] = useState([]);
+
+  useEffect(() => {
+    const options = { method: "GET" };
+
+    fetch("https://jsonplaceholder.typicode.com/users", options)
+      .then((res) => res.json())
+      .then((data) => setAllContact(data))
+      .catch((err) => console.error(err));
+  }, []);
+
+  const handleEven = () => {
+    setCheck(!check);
+    if(check){
+        const even = allContact.filter(contact => contact.id % 2 === 0);
+        setAllContact(even)
+    }
+    else{
+        setAllContact()
+    }
+
+};
 
   return (
     <>
@@ -23,7 +49,7 @@ const Problem2 = () => {
             <button
               variant="primary"
               onClick={handleShowA}
-              className="btn btn-lg btn-outline-primary"
+              className="btn btn-lg btn-outline-primary bg-[#46139f]"
               type="button"
             >
               All Contacts
@@ -50,22 +76,50 @@ const Problem2 = () => {
           <Modal.Title>All Contact</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          I will not close if you click outside me. Don not even try to press
-          escape key.
+          <div>
+            <table className="table table-striped ">
+              <thead>
+                <tr>
+                  <th scope="col">Name</th>
+                  <th scope="col">Phone</th>
+                </tr>
+              </thead>
+              <tbody>
+                {allContact.map((contact, index) => (
+                  <tr key={index}>
+                    <td>{contact.name}</td>
+                    <td>{contact.phone}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleCloseA}>
-            Close
-          </Button>
-          <Button
-            variant="primary"
-            onClick={() => {
-              handleShowB();
-              handleCloseA();
-            }}
-          >
-            US Contact
-          </Button>
+        <Modal.Footer className="d-flex justify-content-between">
+          <div>
+            <Form>
+              <Form.Check
+                onClick={handleEven}
+                type="switch"
+                id="custom-switch"
+                label="Only even"
+              />
+            </Form>
+          </div>
+          <div className="d-flex gap-2">
+            <Button variant="secondary" onClick={handleCloseA}>
+              Close
+            </Button>
+            <Button
+              variant="primary"
+              onClick={() => {
+                handleShowB();
+                handleCloseA();
+              }}
+            >
+              US Contact
+            </Button>
+          </div>
         </Modal.Footer>
       </Modal>
 
@@ -82,11 +136,13 @@ const Problem2 = () => {
           I will not close if you click outside me. Don not even try to press
           escape key.
         </Modal.Body>
-        <Modal.Footer>
-            <div>
-                
-            </div>
+        <Modal.Footer className="d-flex justify-content-between">
           <div>
+            <Form>
+              <Form.Check type="switch" id="custom-switch" label="Only even" />
+            </Form>
+          </div>
+          <div className="d-flex gap-2">
             <Button variant="secondary" onClick={handleCloseB}>
               Close
             </Button>
